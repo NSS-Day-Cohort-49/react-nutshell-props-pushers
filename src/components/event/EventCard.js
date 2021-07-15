6cb9df17ce88b950a3ae8e00c6a26628a77f068d
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-15 15:30:42 -0500

    finished setting up events modules to display events

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import "./Event.css"
+import React from "react";
+import "./Event.css";
 
-
-export const EventCard= () => {
-	return(
-		<>
-
-		</>
-	)
-}
+export const EventCard = ({ event }) => {
+  return (
+    <>
+      <section className="event">
+        <h3 className="event_name">{event.name}</h3>
+        <div className="event_date">Date: {event.date}</div>
+        <div className="event_location">Location: {event.location}</div>
+      </section>
+    </>
+  );
+};
--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import {EventCard} from "./EventCard"
-import "./Event.css"
+import React, { useEffect, useContext } from "react";
+import { EventCard } from "./EventCard";
+import { EventContext } from "./EventProvider";
+import "./Event.css";
 
+export const EventList = () => {
+  const { events, getEvents } = useContext(EventContext);
 
-export const EventList= () => {
-	return(
-		<>
+  useEffect(() => {
+    console.log("useEffect: getEvents");
+    getEvents();
+  }, []);
 
-		</>
-	)
-}
+  return (
+    <>
+      {console.log("EventList - Render: events", events)}
+      {events.map((event) => {
+        return <EventCard key={event.id} event={event} />;
+      })}
+    </>
+  );
+};
