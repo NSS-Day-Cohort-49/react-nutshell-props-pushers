02d99db53e62be9aed22bd8fa40d86d570c44cef
Sam Carbine <scarbine@me.com>
2021-07-15 13:30:17 -0500

    added message provider

--- src/components/message/MessageProvider.js
+++ src/components/message/MessageProvider.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState, createContext} from 'react'
 import "./Message.css"
 
+export const MessageContext = createContext()
 
-export const MessageProvider= () => {
+export const MessageProvider= (props) => {
+
+	const {messages , setMessages}= useState([])
+
+	const getMessages = () => {
+		return fetch("http://localhost:8088/messages")
+		.then(res => res.json())
+		.then(setMessages)
+	}
+
 	return(
-		<>
-
-		</>
+		<MessageContext.Provider value={{messages, getMessages}} >
+			{props.children}
+		</MessageContext.Provider>
 	)
 }
