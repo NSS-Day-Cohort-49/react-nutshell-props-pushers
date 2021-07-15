1d81b0ff66321c161e0685773f3c6f486ac2fd1b
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-15 15:49:25 -0500

    fixed merge conflicts

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,67 +1,42 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route } from "react-router-dom";
 import { MessageList } from "./message/MessageList";
 import { MessageProvider } from "./message/MessageProvider";
 import { EventList } from "./event/EventList";
 import { EventProvider } from "./event/EventProvider";
-=======
-import React from "react"
-import { Route } from "react-router-dom"
-import { ArticleList } from "./article/ArticleList"
-import { ArticleProvider } from "./article/ArticleProvider"
-import { MessageList } from "./message/MessageList"
-import { MessageProvider } from "./message/MessageProvider"
->>>>>>> main
+import { ArticleList } from "./article/ArticleList";
+import { ArticleProvider } from "./article/ArticleProvider";
 
 export const ApplicationViews = () => {
   return (
     <>
-<<<<<<< HEAD
-=======
-    <ArticleProvider>
->>>>>>> main
-      <Route exact path="/">
-        {/* Render the component for news articles */}
-        <ArticleList />
-      </Route>
+      <ArticleProvider>
+        <Route exact path="/">
+          {/* Render the component for news articles */}
+          <ArticleList />
+        </Route>
       </ArticleProvider>
 
       <Route path="/friends">
         {/* Render the component for list of friends */}
       </Route>
-<<<<<<< HEAD
+
       <MessageProvider>
         <Route path="/messages">
           {/* Render the component for the messages */}
           <MessageList />
         </Route>
-=======
-
-    <MessageProvider>
-      <Route path="/messages">
-        {/* Render the component for the messages */}
-        <MessageList />
-      </Route>
->>>>>>> main
       </MessageProvider>
 
       <Route path="/tasks">
         {/* Render the component for the user's tasks */}
       </Route>
-<<<<<<< HEAD
       <EventProvider>
         <Route path="/events">
           {/* Render the component for the user's events */}
           <EventList />
         </Route>
       </EventProvider>
-=======
-
-      <Route path="/events">
-        {/* Render the component for the user's events */}
-      </Route>
->>>>>>> main
     </>
   );
 };
--- src/components/message/MessageCard.js
+++ src/components/message/MessageCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Message.css";
 
-<<<<<<< HEAD
 export const MessageCard = ({ message }) => {
   return (
     <>
@@ -13,18 +12,3 @@ export const MessageCard = ({ message }) => {
     </>
   );
 };
-=======
-
-export const MessageCard = ({message}) => {
-	return(
-		<>
-		<section className="message">
-			<h3 className="message_title">{message.title}</h3>
-			<div>From: {message.user.name}</div>
-			<div className="message_message">{message.message}</div>
-		</section>
-
-		</>
-	)
-}
->>>>>>> main
