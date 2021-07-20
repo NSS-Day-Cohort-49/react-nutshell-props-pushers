7de852cbcbc05a28c85b227198727283bc9226bd
sxiong0519 <sxiong0519@gmail.com>
2021-07-20 15:52:54 -0500

    added unfriend/add friend buttons to allusers page

--- src/components/user/UserCard.js
+++ src/components/user/UserCard.js
@@ -1,58 +1,38 @@
 import React, { useContext, useEffect } from "react";
 import "./User.css";
-import { UserContext } from "./UserProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FriendContext } from "../friends/FriendProvider";
-import { useHistory, useParams } from "react-router";
 
 export const UserCard = ({ user }) => {
   
-  // const {users, addUser, deleteUser} = UseContext(UserContext);
-  
-  
-  // const addNewUser = () => {
-  //   const newUserObj = {
-  //     currentUserId: parseInt(sessionStorage.getItem("nutshell_user")),
-  //     userId: userId,
-  //   };
-
-  //   addUser(newUserObj);
-  // };
-
-  // const User = () => {
-  //   deleteUser(foundUser.id);
-  // };
-
   const currentUser = parseInt(sessionStorage.getItem("nutshell_user"));
   const { friends, getFriends, addFriend, deleteFriend } =
     useContext(FriendContext);
   // add addFriends when provider is built
 
-  const userId = useParams();
-  const history = useHistory()
-
   useEffect(() => {
     getFriends();
   }, []);
 
-  let foundFriend = friends.find((friend)=> (currentUser === friend.currentUserId && friend.userId === user.id) )
-  
+  let foundFriend = friends.find((friend)=> (currentUser === friend.currentUserId && friend.userId === user.id))
+
   let friendStyling = "not-friend"
  if (foundFriend) {
    friendStyling = "friend"
  } 
-  const handleClickFriend = () => {
-    if (userId) {
-      deleteFriend(foundFriend.Id)
-    } else {
-      const newFriendObj = {
-        currentUserId: currentUser,
-        userId: userId,
-      }
-      addFriend(newFriendObj)
-      .then(() => history.push("/friends"))
-    }
-  }
+
+  const addNewFriend = () => {
+    const newFriendObj = {
+      currentUserId: currentUser,
+      userId: user.id,
+    };
+
+    addFriend(newFriendObj);
+  };
+  
+  const unfriend = () => {
+    deleteFriend(foundFriend.id);
+  };
 
 
   return (
@@ -67,8 +47,20 @@ export const UserCard = ({ user }) => {
             /> */}
             <h5 className="card-title">{user.name}</h5>
             <div className="card-text">{user.email}</div>
-            <div><button onClick={handleClickFriend}>
-              {foundFriend ? <>Unfriend</> : <>Add Friend</>}</button></div>
+            <div>            
+            {foundFriend ? (
+              <>
+                <button className={friendStyling} onClick={unfriend}>
+                  Unfriend
+                </button>
+              </>
+            ) : (
+              <>
+                <button className={friendStyling} onClick={addNewFriend}>
+                  Add Friend
+                </button>
+              </>
+            )}</div>
           </div>
         </div>
       </div>
