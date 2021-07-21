54f0ebdc0c1727962e658f24d3c02acbaa1e02f4
Sam Carbine <scarbine@me.com>
2021-07-21 15:04:29 -0500

    format date to string

--- src/components/weather/WeatherCard.js
+++ src/components/weather/WeatherCard.js
@@ -3,16 +3,14 @@ import { WeatherContext } from './WeatherProvider'
 
 export const WeatherCard = ({w}) => {
 
-	
-
-	
+	const date = Date(w.dt)
 
 	return(
 		<>
 		<section className="weather">
 		<h3>Current Weather</h3>
 		{console.log(w)}
-		{/* <div>This is the forecast for {w.list.dt}</div> */}
+		<div>This is the forecast for this date {date.toString()}</div>
 		</section>
 		</>
 	)
