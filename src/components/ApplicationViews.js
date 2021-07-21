d1509fa9e0ceec45cfc696bbf991b45221c58aa7
sxiong0519 <sxiong0519@gmail.com>
2021-07-21 11:51:46 -0500

    filtered articles feed to only show currentUser and friends

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -22,7 +22,8 @@ export const ApplicationViews = () => {
   return (
     <>
       <ArticleProvider>
-        <FriendProvider><Route exact path="/">
+        <FriendProvider>
+          <Route exact path="/">
           {/* Render the component for news articles */}
           <ArticleList />
         </Route>
